4fe8c07481b32a7ad37fe1c70ae041a3c84b26be
Joshua Avrick <josh.avrick@gmail.com>
2019-05-20 00:24:10 -0400

    removed unused resources

--- src/util/convert-range.js
+++ src/util/convert-range.js
@@ -1,3 +1,14 @@
+
+/**
+ * Given a number on a particular range, this function rescales the number for a 
+ * new (ostensibly smaller or larger) range of numbers
+ * 
+ * @param {*} oldVal - The value to rescale
+ * @param {*} oldMin - The original minimum range value
+ * @param {*} oldMax - The original maximum range value
+ * @param {*} newMin - The new range minimum 
+ * @param {*} newMax - The new range maximum 
+ */
 export default function convertRange(oldVal, oldMin, oldMax, newMin, newMax){
 	return (((oldVal - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
 }
