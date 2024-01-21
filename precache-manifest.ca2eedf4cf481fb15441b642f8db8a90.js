0202ecd1e0b00940bf61a4b82005256ed81940d6
jhavrick <josh.avrick@gmail.com>
2024-01-20 21:36:59 -0500

    Updates

---
+++ precache-manifest.ca2eedf4cf481fb15441b642f8db8a90.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d85589ba6fd865c54949",
+    "url": "/static/css/main.33e8e6a8.chunk.css"
+  },
+  {
+    "revision": "d85589ba6fd865c54949",
+    "url": "/static/js/main.8ed32146.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7d798e1b4f8f0ce7e15",
+    "url": "/static/js/2.e15a5a87.chunk.js"
+  },
+  {
+    "revision": "38a86a327a5a888caf33b62b8cab7d1d",
+    "url": "/static/media/volca-simple.38a86a32.gif"
+  },
+  {
+    "revision": "0b989f0b700fd3547c453fcb95a44251",
+    "url": "/static/media/volca.0b989f0b.png"
+  },
+  {
+    "revision": "c6cdfded4630ba6d9a2dceb70aa4fe0f",
+    "url": "/static/media/Roboto-Light.c6cdfded.ttf"
+  },
+  {
+    "revision": "9e2688278bcb7a199bcfd8c8302eade3",
+    "url": "/static/media/Moon-Light.9e268827.otf"
+  },
+  {
+    "revision": "63f874d192fb3892d88d5e26f942b5e2",
+    "url": "/static/media/DS-DIGI.63f874d1.TTF"
+  },
+  {
+    "revision": "8523ae6f341702e4e5a9b455377798e7",
+    "url": "/index.html"
+  }
+];
